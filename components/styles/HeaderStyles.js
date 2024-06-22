415432cc70a0ce87a0dcd9dc597afde4e6c16080
Sarita-nangi <145735180+Sarita-nangi@users.noreply.github.com>
2024-06-22 10:25:13 +0530

    Create HeaderStyles.js

---
+++ components/styles/HeaderStyles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  text-align: center;
+  padding: 20px;
+  background-color: #f5f5f5;
+`;
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+  margin: 0;
+`;
+
+export const Description = styled.p`
+  font-size: 1.2rem;
+  color: #555;
+`;
