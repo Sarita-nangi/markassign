50937b57d6efe4dfb9c3a6ecee8aba7becca2d80
Sarita-nangi <145735180+Sarita-nangi@users.noreply.github.com>
2024-06-22 10:26:06 +0530

    Create ChartStyles.js

---
+++ components/styles/ChartStyles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ChartContainer = styled.div`
+  margin: 20px;
+  padding: 20px;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
